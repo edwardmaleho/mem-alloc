71ca38a276436428547a918bca224bec93eddd67
Edward Maleho <Edward.Maleho@dsa.de>
2025-05-31 14:12:05 +0200

    basic functionality works

--- main.c
+++ main.c
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 
+#define PREV_INUSE 0x01
+#define MIN_SIZE 0xFF
+
+#define MIN_CHUNK_SIZE 32
+
+typedef struct {
+    size_t __prev_size;
+    size_t size;
+} meta_data;
+
+typedef struct free_chunk {
+    meta_data __meta_data;
+    struct free_chunk* fd;
+    struct free_chunk* bk;
+ } free_chunk;
+
 void* map = NULL;
 
-struct {
-    void *__next_ptr;
-    size_t size;
-} typedef meta_data;
+free_chunk* free_head;
 
 void init_map(size_t map_size) {
     map = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -15,25 +28,50 @@ void init_map(size_t map_size) {
         printf("Failed to create map");
         exit;
     }
+    meta_data* __free_list = map;
+    __free_list->__prev_size = 0;
+    __free_list->size = map_size;
+
+    free_head = map;
+    free_head->__meta_data.size = map_size;
+    free_head->__meta_data.__prev_size = 0;
+    free_head->fd = NULL;
+    free_head->bk = NULL;
+
+}
+
+void new_free_head(free_chunk** __new_head) {
+    free_head->bk = *__new_head;
+    (*__new_head)->fd = free_head;
+    (*__new_head)->bk = NULL;
+    free_head = *__new_head;
 }
 
 void *mem_malloc(size_t __size) {
     if (map == NULL) {
         init_map(4096);
     }
-    size_t meta_size = sizeof(meta_data);
-    // void* __ptr = malloc(meta_size + __size);
-    void* __ptr = map;
-    meta_data *__meta = __ptr;
-    __meta->size = __size;
-    __meta->__next_ptr = NULL;
-    printf("meta: size: %ld next: %p\n", __meta->size, __meta->__next_ptr);
-    return (void*)((char*)__ptr + meta_size/8);
+    if (free_head->__meta_data.size > __size) {
+        size_t head_size = free_head->__meta_data.size;
+        meta_data* __meta = (meta_data*)free_head;
+        __meta->size = (__size + 15) & ~0xF;
+        __meta->__prev_size = 0;
+        if (head_size - __meta->size > MIN_CHUNK_SIZE) {
+            free_chunk* __new_head = (free_chunk*)((char*)(__meta+1) + (__meta->size));
+            new_free_head(&__new_head);
+        }
+        return (void*)(__meta + 1);
+    } else {
+        return NULL;
+    }
+    
 }
 
 void mem_free(void *__ptr) {
-    void *__meta = (void*)((char*)__ptr-2);
-    // free(__meta);
+    meta_data* __meta = ((meta_data*)__ptr-1);
+    free_chunk* __new_head = (free_chunk*)__meta;
+    new_free_head(&__new_head);
+    __new_head->__meta_data.size = __meta->size;
 }
 
 void create_nums(int** nums, size_t size) {
@@ -56,7 +94,14 @@ int main() {
     create_nums(&nums, size);
     print_nums(nums, size);
     mem_free(nums);
+    int* nums2;
+    size_t size2 = 40;
+    create_nums(&nums2, size2);
+    print_nums(nums2, size2);
+    mem_free(nums2);
+    print_nums(nums2, size2);
     munmap(map, 4096);
+    // printf("%ld\n", sizeof(free_chunk));
 
     return 0;
 }
